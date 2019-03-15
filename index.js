5609c3bc29b86f68f5367a5ef12a9ae51b7928a2
Mathias Biilmann Christensen <info@mathias-biilmann.net>
2019-03-15 09:07:58 -0700

    Export parseTomlFormat

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const lineParser = require('./line-parser')
+const tomlParser = require('./toml-parser')
 
 exports.parseRedirectsFormat = lineParser.parse
+exports.parseTomlFormat = tomlParser.parse
