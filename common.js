97ff75b614398d465b637b1c8658a37df5514262
Raees Iqbal <raees.bhatti@gmail.com>
2019-11-07 00:29:03 +0500

    Add YAML parser

--- common.js
+++ common.js
@@ -9,7 +9,76 @@ function parseURL(url) {
   return URLclass.parse(url)
 }
 
-module.exports = {
+function splatForwardRule(path, obj, dest) {
+  return (
+    path.match(/\/\*$/) &&
+    dest == null &&
+    obj.status &&
+    obj.status >= 200 &&
+    obj.status < 300 &&
+    obj.force
+  )
+}
+
+function isPlainObj(o) {
+  return typeof o == 'object' && o.constructor == Object
+}
+
+function fetch(obj, options) {
+  for (const i in options) {
+    if (obj.hasOwnProperty(options[i])) {
+      return obj[options[i]]
+    }
+  }
+  return null
+}
+
+function redirectMatch(obj) {
+  const origin = fetch(obj, ['from', 'origin'])
+  const redirect =
+    origin && origin.match(exp.FULL_URL_MATCHER)
+      ? exp.parseFullOrigin(origin)
+      : { path: origin }
+  if (redirect == null || (redirect.path == null && redirect.host == null)) {
+    return null
+  }
+
+  const dest = fetch(obj, ['to', 'destination'])
+  if (splatForwardRule(redirect.path, obj, dest)) {
+    redirect.to = redirect.path.replace(/\/\*$/, '/:splat')
+  } else {
+    redirect.to = dest
+  }
+
+  if (redirect.to == null) {
+    return null
+  }
+
+  redirect.params = fetch(obj, ['query', 'params', 'parameters'])
+  redirect.status = fetch(obj, ['status'])
+  redirect.force = fetch(obj, ['force'])
+  redirect.conditions = fetch(obj, ['conditions'])
+  redirect.headers = fetch(obj, ['headers'])
+  redirect.signed = fetch(obj, ['sign', 'signing', 'signed'])
+
+  Object.keys(redirect).forEach(key => {
+    if (redirect[key] === null) {
+      delete redirect[key]
+    }
+  })
+
+  if (redirect.headers && !isPlainObj(redirect.headers)) {
+    return null
+  }
+
+  return redirect
+}
+
+const exp = {
+  splatForwardRule,
+  isPlainObj,
+  redirectMatch,
+
   FULL_URL_MATCHER: /^(https?):\/\/(.+)$/,
   FORWARD_STATUS_MATCHER: /^2\d\d!?$/,
 
@@ -37,3 +106,5 @@ module.exports = {
     }
   },
 }
+
+module.exports = exp
