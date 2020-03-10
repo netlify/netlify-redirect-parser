d362a6b23f67cc0163d7645068e3903c5db64422
Raees Iqbal <raees.bhatti@gmail.com>
2020-03-09 18:24:45 -0700

    Add test for netlify.yml

--- netlify-config-parser.test.js
+++ netlify-config-parser.test.js
@@ -38,3 +38,37 @@ test('netlify.toml redirects parsing', async t => {
     result.success
   )
 })
+
+test('netlify.yml redirects parsing', async t => {
+  const result = await parser.parse(path.resolve(testFilesDir, 'netlify.yml'))
+  t.deepEqual(
+    [
+      {
+        path: '/old-path',
+        to: '/new-path',
+        status: 301,
+        conditions: {
+          Country: ['US'],
+          Language: ['en'],
+          Role: ['admin'],
+        },
+        force: false,
+        params: {
+          path: ':path',
+        },
+      },
+      {
+        path: '/search',
+        to: 'https://api.mysearch.com',
+        status: 200,
+        force: true,
+        signed: 'API_SIGNATURE_TOKEN',
+        proxy: true,
+        headers: {
+          'X-From': 'Netlify',
+        },
+      },
+    ],
+    result.success
+  )
+})
